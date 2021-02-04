1cd2c1977064eb41967b76fe32e843581ad9a26d
zqinmiao <zqinmiao@gmail.com>
2021-02-04 13:48:36 +0800

    feat: stylelint增加兼容css modules的语法

--- stylelint.js
+++ stylelint.js
@@ -1,5 +1,30 @@
 // https://stylelint.io/
 module.exports = {
   extends: ["stylelint-config-recommended", "stylelint-config-standard"],
-  rules: {},
+  rules: {
+    // 禁止使用未知的伪类选择器：https://stylelint.io/user-guide/rules/selector-pseudo-class-no-unknown
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        // 忽略css modules语法
+        ignorePseudoClasses: ["global", "local", "pure"],
+      },
+    ],
+    // 禁止使用未知属性：https://stylelint.io/user-guide/rules/property-no-unknown
+    "property-no-unknown": [
+      true,
+      {
+        // 忽略css modules语法
+        ignoreProperties: ["composes"],
+      },
+    ],
+    // 禁止使用未知规则：https://stylelint.io/user-guide/rules/at-rule-no-unknown
+    "at-rule-no-unknown": [
+      true,
+      {
+        // 忽略css modules语法
+        ignoreAtRules: ["value"],
+      },
+    ],
+  },
 };
--- tests/typescript/rules/index.ts
+++ tests/typescript/rules/index.ts
@@ -5,4 +5,13 @@
  */
 const __a = 1;
 
+const object = { x: 42, y: 50 };
+const entries = Object.entries(object);
+// → [['x', 42], ['y', 50]]
+console.log("entries", entries);
+
+const result = Object.fromEntries(entries);
+// → { x: 42, y: 50 }
+console.log("result", result);
+
 export {};
